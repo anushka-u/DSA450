5345bc6f6443909c949177b4f0e3da856a8cb8df
anushka-u <anushkau032@gmail.com>
2024-01-20 14:43:25 +0530

    calling custom arraylistinsert() remove() and toString() method

--- Collections/ArrayList/com/in/customarraylist/Main.java
+++ Collections/ArrayList/com/in/customarraylist/Main.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        CustomArrayList arr = new CustomArrayList(2);
+        CustomArrayList arr = new CustomArrayList(5);
         arr.insert(2);
         arr.insert(7);
         arr.insert(4);
         arr.insert(42);
         arr.insert(81);
-        arr.PrintArray();
         System.out.println();
         int a = (int) arr.remove(4);
+        arr.insert(46);
         System.out.println(a);
-        arr.PrintArray();
+        System.out.println(arr.toString());
        }
 }
