de3f181a92912fb33a4969bda6a465ef04fd713a
anushka-u <anushkau032@gmail.com>
2024-01-21 21:54:22 +0530

    Create an anonymous method to sort thye employee data by salary

--- ArrayProblems/com/in/sorting/Main.java
+++ ArrayProblems/com/in/sorting/Main.java
@@ -2,6 +2,7 @@ package com.in.sorting;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -10,12 +11,12 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		List<Employee> emp = new ArrayList<>();
-		emp.add(new Employee(121, "Kartik"));
-		emp.add(new Employee(120, "Kirti"));
-		emp.add(new Employee(119, "Kirtika"));
-		emp.add(new Employee(122, "Karan"));
-		emp.add(new Employee(125, "Kusum"));
-		emp.add(new Employee(126, "Kanak"));
+		emp.add(new Employee(121, "Kartik",34908.00));
+		emp.add(new Employee(120, "Kirti",4536210.98));
+		emp.add(new Employee(119, "Kirtika",98765.987));
+		emp.add(new Employee(122, "Karan",876543.09));
+		emp.add(new Employee(125, "Kusum",2345213.09));
+		emp.add(new Employee(126, "Kanak",56437.90));
 
 		//It will use Employee class comparator which will sort data as per id
 		Collections.sort(emp);
@@ -26,6 +27,13 @@ public class Main {
 		System.out.println(emp);
 
 
+		Collections.sort(emp, new Comparator<Employee>() {
+			@Override
+			public int compare(Employee o1, Employee o2) {
+				return (o1.getSalary()>o2.getSalary())?1 : (o1.getSalary()==o2.getSalary() ? 0:-1);
+			}
+		});
+		System.out.println(emp);
 		
 		
 		
