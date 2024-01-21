116a7d073941f28c7975becb7cc200b62d636822
anushka-u <anushkau032@gmail.com>
2024-01-21 19:57:48 +0530

    Override compareTo method to sort the id in ascending order

--- ArrayProblems/com/in/sorting/Employee.java
+++ ArrayProblems/com/in/sorting/Employee.java
@@ -4,7 +4,23 @@ public class Employee implements Comparable<Employee> {
 
 	private int empId;
 	private String empName;
-	
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
 	public Employee(int id, String name)
 	{
 		this.empId=id;
@@ -13,7 +29,7 @@ public class Employee implements Comparable<Employee> {
 	
 	public int compareTo(Employee o) {
 		//(x < y) ? -1 : ((x == y) ? 0 : 1);
-		return (o.empId<this.empId)?-1:((o.empId==this.empId)?0:1);
+		return (o.empId<this.empId)?1:((o.empId==this.empId)?0:-1);
 	}
 	
 	@Override
