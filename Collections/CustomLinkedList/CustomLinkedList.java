1df0430a9922979239245a40d4930a98d20262e0
anushka-u <anushkau032@gmail.com>
2024-01-27 00:15:56 +0530

    Modifying add method

--- Collections/CustomLinkedList/CustomLinkedList.java
+++ Collections/CustomLinkedList/CustomLinkedList.java
@@ -1,13 +1,17 @@
+import java.util.StringJoiner;
 
 public class CustomLinkedList {
 
-    Node head = null;
+    Node head;
+    Node last;
     public static void main(String[] args) {
 
         CustomLinkedList cll = new CustomLinkedList();
         cll.add(10);
         cll.add(40);
         cll.add(56);
+
+        System.out.println(cll);
     }
 
     public class Node {
@@ -24,16 +28,22 @@ public class CustomLinkedList {
             if(head == null)
             {
                 head = n;
+                last=n;
             }
             else{
-                Node temp = head;
-                while(temp.next!=null)
-                {
-                    temp =temp.next;
-                }
-                temp.next=n;
+
+                last.next=n;
+                last=n;
+//                Node temp = head;
+//                while(temp.next!=null)
+//                {
+//                    temp =temp.next;
+//                }
+//                temp.next=n;
 
             }
 
     }
+
+
 }
