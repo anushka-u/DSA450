bbf365da0b58eff7820a209a9d8f9d47afab8647
anushka-u <anushkau032@gmail.com>
2024-04-14 14:09:30 +0530

    Collections checkedCollection() method demo

--- CollectionsUtilityClass/checkedCollectionDemo.java
+++ CollectionsUtilityClass/checkedCollectionDemo.java
@@ -18,5 +18,18 @@ public class checkedCollectionDemo {
             System.out.println("Cannot add, it's type safed");
         }
         System.out.println(collection);
+
+        List l2 = new ArrayList();
+        l2.add("Suman");
+        l2.add(12);
+
+        Collection<String> checkedCollection = Collections.checkedCollection(l2,String.class);
+        try{
+            checkedCollection.add("Sutum");
+        }catch (ClassCastException e)
+        {
+            System.out.println("Cannot add as l2 is typesafe now");
+        }
+        System.out.println(checkedCollection);
     }
 }
