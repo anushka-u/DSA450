d579ce6b75532aed39619522c87af3c77070dd01
anushka-u <anushkau032@gmail.com>
2024-12-08 02:30:17 +0530

    Recurssion : Reversing a String

--- Recurssion/ReverseString/ReverseString.java
+++ Recurssion/ReverseString/ReverseString.java
@@ -21,10 +21,19 @@ public class ReverseString {
         revString = str.charAt(idx)+revString;
         return ReverseStringStr(str,idx+1,revString);
     }
+
+    public static String Reversing(String str)
+    {
+        if(str.isEmpty())
+            return revString;
+        revString=revString+str.charAt(str.length()-1);
+        return Reversing(str.substring(0,str.length()-1));
+    }
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         System.out.println(ReverseStringStr(str,0, revString));
+        System.out.println(Reversing(str));
     }
 }
