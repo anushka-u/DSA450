a9daa45026216f443d80d5548191ea1f52c995e4
anushka-u <anushkau032@gmail.com>
2024-12-14 03:05:03 +0530

    Recursion : Level Easy

--- RecursionDynamicProgrammingAndBackTracking/recursion/easy/CountVowels.java
+++ RecursionDynamicProgrammingAndBackTracking/recursion/easy/CountVowels.java
@@ -2,12 +2,13 @@ package recursion.easy;
 
 public class CountVowels {
 
-    static int count =0;
+    static int count=0;
 
-    public static void countVowel(String str){
+
+    public static int  countVowel(String str){
         if(str.isEmpty())
         {
-            return;
+            return count;
         }
         if(str.charAt(str.length()-1)=='a'||str.charAt(str.length()-1)=='e'||
                 str.charAt(str.length()-1)=='i'||str.charAt(str.length()-1)=='o'
@@ -15,12 +16,28 @@ public class CountVowels {
         {
             count+=1;
         }
-        countVowel(str.substring(0,str.length()-1));
+        return countVowel(str.substring(0,str.length()-1));
+    }
+
+    public static int countConsonants(String str)
+    {
+        if(str.isEmpty())
+        {
+            return count;
+        }
+        if(str.charAt(str.length()-1)!='a'&& str.charAt(str.length()-1)!='e'&&
+            str.charAt(str.length()-1)!='i'&& str.charAt(str.length()-1)!='o'
+            &&str.charAt(str.length()-1)!='u'){
+            count+=1;
+        }
+        return countConsonants(str.substring(0,str.length()-1));
     }
 
     public static void main(String[] args) {
         String str ="anushka";
-        countVowel(str);
+        countConsonants(str);
         System.out.println(count);
+
+
     }
 }
