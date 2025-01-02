78e9ebcc0d49d928fcb08624ce013010844ed318
anushka-u <anushkau032@gmail.com>
2025-01-02 23:34:28 +0530

    Stack - Stock Span Problem.

--- stack/InterviewQuestions/stockSpanProblem.java
+++ stack/InterviewQuestions/stockSpanProblem.java
@@ -34,6 +34,7 @@ public class stockSpanProblem {
     public static void main(String[] args) {
         int[] arr = {100,80,60,70,60,75,85};
         int[] result = stackSpan(arr);
+
         for (int i : result) {
             System.out.print(i+" ");
         }
